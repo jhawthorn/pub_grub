5abec8a05cd74c588abe7ba1cc585ec5e912640f
John Hawthorn <john@hawthorn.email>
2018-11-25 19:04:49 -0800

    Start phasing out package.versions

--- lib/pub_grub/static_package_source.rb
+++ lib/pub_grub/static_package_source.rb
@@ -27,14 +27,20 @@ module PubGrub
       @packages = {
         root: Package.root
       }
-      @deps_by_version = {
-        Package.root_version => @root_deps
-      }
+      @package_versions = Hash.new{ |h, k| h[k] = [] }
+
+      @deps_by_version = Hash.new { |h, k| h[k] = {} }
+
+      root_version = "1.0.0"
+      @package_versions[Package.root] = [root_version]
+      @deps_by_version[Package.root][root_version] = @root_deps
 
       @package_list.each do |name, version, deps|
         @packages[name] ||= Package.new(name)
-        version = @packages[name].add_version(version)
-        @deps_by_version[version] = deps
+        package = @packages[name]
+        @package_versions[name] << version
+        @packages[name].add_version(version)
+        @deps_by_version[package][version] = deps
       end
     end
 
@@ -48,7 +54,7 @@ module PubGrub
       package(package_name).version(version_name)
     end
 
-    def versions_for(package, range)
+    def versions_for(package, range=VersionRange.any)
       package.versions.select do |version|
         range.include?(Gem::Version.new(version.name))
       end
@@ -56,36 +62,37 @@ module PubGrub
 
     def incompatibilities_for(version)
       package = version.package
-      @deps_by_version[version].map do |dep_package_name, dep_constraint_name|
+      package_deps = @deps_by_version[package]
+      package_deps[version.name].map do |dep_package_name, dep_constraint_name|
         self_constraint =
           if package.versions == [version]
             VersionConstraint.any(package)
           else
-            sorted_versions = package.versions.sort_by { |v| Gem::Version.new(v.name) }
-            low = high = sorted_versions.index(version)
+            sorted_versions = package.versions.sort_by { |v| Gem::Version.new(v.name) }.map(&:to_s)
+            low = high = sorted_versions.index(version.name)
 
             # find version low such that all >= low share the same dep
             while low > 0 &&
-                @deps_by_version[sorted_versions[low - 1]][dep_package_name] == dep_constraint_name
+                package_deps[sorted_versions[low - 1]][dep_package_name] == dep_constraint_name
               low -= 1
             end
             low =
               if low == 0
                 nil
               else
-                Gem::Version.new(sorted_versions[low].name)
+                Gem::Version.new(sorted_versions[low])
               end
 
             # find version high such that all < high share the same dep
             while high < sorted_versions.length &&
-                @deps_by_version[sorted_versions[high]][dep_package_name] == dep_constraint_name
+                package_deps[sorted_versions[high]][dep_package_name] == dep_constraint_name
               high += 1
             end
             high =
               if high == sorted_versions.length
                 nil
               else
-                Gem::Version.new(sorted_versions[high].name)
+                Gem::Version.new(sorted_versions[high])
               end
 
             range = VersionRange.new(min: low, max: high, include_min: true)
