9250b271eeccdb977091b602a49d55de978e1f24
John Hawthorn <john@hawthorn.email>
2018-12-24 02:55:44 -0800

    Simplify StaticPackageSource

--- lib/pub_grub/static_package_source.rb
+++ lib/pub_grub/static_package_source.rb
@@ -16,46 +16,36 @@ module PubGrub
       end
 
       def add(name, version, deps: {})
-        @packages << [name, Gem::Version.new(version), deps]
+        version = Gem::Version.new(version)
+        @packages[name] ||= {}
+        raise ArgumentError, "#{name} #{version} declared twice" if @packages[name].key?(version)
+        @packages[name][version] = deps
       end
     end
 
     def initialize
       @root_deps = {}
-      @package_list = []
-      yield DSL.new(@package_list, @root_deps)
+      @packages = {}
 
-      @packages = Set.new([Package.root])
-      @package_versions = Hash.new { |h, k| h[k] = [] }
-      @deps_by_version = Hash.new { |h, k| h[k] = {} }
-
-      root_version = Package.root_version
-      @package_versions[Package.root] = [root_version]
-      @deps_by_version[Package.root][root_version] = @root_deps
-
-      @package_list.each do |package, version, deps|
-        @packages.add(package)
-        @package_versions[package] << version
-        @deps_by_version[package][version] = deps
-      end
+      yield DSL.new(@packages, @root_deps)
 
       super()
     end
 
     def all_versions_for(package)
-      @package_list.select do |pkg, _, _|
-        pkg == package
-      end.map do |_, version, _|
-        version
-      end
+      @packages[package].keys
+    end
+
+    def root_dependencies
+      @root_deps
     end
 
     def dependencies_for(package, version)
-      @deps_by_version[package][version]
+      @packages[package][version]
     end
 
     def parse_dependency(package, dependency)
-      return false unless @packages.include?(package)
+      return false unless @packages.key?(package)
 
       PubGrub::RubyGems.parse_constraint(package, dependency)
     end
