5d3b8207989b6a8bb557cd007c38eb874916c392
John Hawthorn <john@hawthorn.email>
2019-01-03 22:30:51 -0800

    Fix warning

--- test/pub_grub/version_test.rb
+++ test/pub_grub/version_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 module PubGrub
   class VersionTest < Minitest::Test
     def test_valid
-      assert_match /\A\d+\.\d+\.\d+\z/, PubGrub::VERSION
+      assert_match(/\A\d+\.\d+\.\d+\z/, PubGrub::VERSION)
     end
   end
 end
