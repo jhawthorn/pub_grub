88513e8567ee4b1ccb7ccea5344d8fee807f6dd9
John Hawthorn <john@hawthorn.email>
2019-01-07 18:08:16 -0800

    Allow our own version to be a prerelease

--- test/pub_grub/version_test.rb
+++ test/pub_grub/version_test.rb
@@ -3,7 +3,7 @@ require "test_helper"
 module PubGrub
   class VersionTest < Minitest::Test
     def test_valid
-      assert_match(/\A\d+\.\d+\.\d+\z/, PubGrub::VERSION)
+      assert_match(/\A\d+\.\d+\.\d+(\.[a-zA-Z0-9]+)*\z/, PubGrub::VERSION)
     end
   end
 end
